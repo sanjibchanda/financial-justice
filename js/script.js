a1597990fbfe7b0cf1da2416dd6c02db252bc0c6
sanjibchanda <sanjibchanda07@gmail.com>
2022-01-16 10:15:18 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,94 @@
+//preloader
+$(window).bind("load", function () {
+  jQuery("#loader").fadeOut("slow");
+  jQuery("#preloader").delay(0).fadeOut();
+});
+
+/*responsive slider*/
+$(".responsive").slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+//sticky navbar
+// window.onscroll = function () {
+//   myFunction();
+// };
+// var header = document.getElementById("header");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+//scroll to top script
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".scrollup").fadeIn();
+    } else {
+      $(".scrollup").fadeOut();
+    }
+  });
+
+  $(".scrollup").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      600
+    );
+    return false;
+  });
+});
+
+//AOS script
+
+AOS.init({
+  offset: 100, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1000, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
